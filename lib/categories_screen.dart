7e21cd382c833d0fe5bb70ec18784ed89fa51a92
Mateusz Konopka (W) <mkonopkadev@gmail.com>
2022-02-17 15:51:01 +0100

    open new screen & passing data between screens

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -15,6 +15,7 @@ class CategoriesScreen extends StatelessWidget {
           padding: EdgeInsets.all(10),
           children: DUMMY_CATEGORIES
               .map((catData) => CategoryItem(
+                    catData.id,
                     catData.title,
                     catData.color,
                   ))
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,24 +1,40 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:gamelib_app/category_games_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return CategoryGamesScreen(
+        categoryId: id,
+        categoryTitle: title,
+      );
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(15)),
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Colors.white,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [color.withOpacity(0.7), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
+      ),
     );
   }
 }
