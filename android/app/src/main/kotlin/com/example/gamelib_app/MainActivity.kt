f694b5de3c847cc05f83ef7e3b6f7346a7c11b8b
Mateusz Konopka (W) <mkonopkadev@gmail.com>
2022-02-17 14:21:48 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/gamelib_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gamelib_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Games Library',
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Games Library'),
+      ),
+      body: Center(
+        child: Text('Navigation Time!'),
+      ),
+    );
+  }
+}
